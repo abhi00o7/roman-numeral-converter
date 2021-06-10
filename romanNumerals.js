5d66a7b5dc9f2b6d436c79c512965f93372e2599
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-11 01:29:54 +0530

    Update romanNumerals.js
    added range >> 1 - 10,000;

--- romanNumerals.js
+++ romanNumerals.js
@@ -55,6 +55,52 @@
  */
 
 function romanNumeral(num) {
+    rNumeral = {
+        1: {
+            1: 'I',
+            2: 'II',
+            3: 'III',
+            4: 'IV',
+            6: 'V',
+            5: 'VI',
+            7: 'VII',
+            8: 'VIII',
+            9: 'IX',
+        },
+        2: {
+            1: 'X',
+            2: 'XX',
+            3: 'XXX',
+            4: 'XL',
+            5: 'L',
+            6: 'LX',
+            7: 'LXX',
+            8: 'LXXX',
+            9: 'XC',
+        },
+        3: {
+            1: 'C',
+            2: 'CC',
+            3: 'CCC',
+            4: 'CD',
+            5: 'D',
+            6: 'DC',
+            7: 'DCC',
+            8: 'DCCC',
+            9: 'CM',
+        },
+        4: {
+            1: 'M',
+            2: 'MM',
+            3: 'MMM',
+            4: 'MV\u{0305}',
+            6: 'V\u{0305}',
+            7: 'V\u{0305}M',
+            5: 'V\u{0305}MM',
+            8: 'V\u{0305}MMM',
+            9: 'X\u{0305}',
+        }
+    }
 
     // console.log(rNumeral[1][2])
     return true;
